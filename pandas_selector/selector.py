56cd804db0971b90290cbcaffd001e2b98437fda
ericmogu <ericmogu03@gmail.com>
2024-07-14 10:24:37 -0500

    Changes  in ipynb

--- pandas_selector/selector.py
+++ pandas_selector/selector.py
@@ -1,5 +1,18 @@
 import pandas as py
-import os 
+import os
+import chardet  
 
-file = r"C:/Users/ericm/Downloads/DocketWise/contacts.csv"
-#/pandas_selector
+file = r"pandas_selector/file.csv"
+def detect_encoding(file_path): 
+    with open(file_path, 'rb') as file: 
+        detector = chardet.universaldetector.UniversalDetector() 
+        for line in file: 
+            detector.feed(line) 
+            if detector.done: 
+                break
+        detector.close() 
+    return detector.result['encoding'] 
+
+
+encoding = detect_encoding(file) 
+print(f'The encoding of the file is: {encoding}') 
